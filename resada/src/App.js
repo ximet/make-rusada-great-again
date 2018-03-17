540d811ee36d77051ad933f1585b9e2b79abc13f
ximet <dq.ximet@gmail.com>
2018-03-17 13:47:55 +0300

    add ui kit

--- resada/src/App.js
+++ resada/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './assets/rings.gif';
+import Button from 'antd/lib/button';
+import 'antd/lib/button/style/index.css'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <img src={logo} className="App-logo" alt="logo" />
+
+          <div className="content">
+          <Button type="primary">Hello world!</Button>
+          </div>
       </div>
     );
   }
