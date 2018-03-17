ad47e472edd27f477642378e1a85b613e08ce649
ximet <dq.ximet@gmail.com>
2018-03-17 13:16:15 +0300

    add icon

--- resada/src/App.js
+++ resada/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './assets/circles.gif';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
