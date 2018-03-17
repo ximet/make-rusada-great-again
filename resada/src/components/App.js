f93cbc14306f7de8b864a421ab2a4e6127fb6fde
Anton Vlasik <antonvlasik@coherentsolutions.com>
2018-03-17 16:15:39 +0300

    attmet to send result

--- resada/src/components/App.js
+++ resada/src/components/App.js
@@ -8,6 +8,7 @@ import Col from 'antd/lib/col';
 import Row from 'antd/lib/row';
 import routesConfig from '../routing/routesConfig';
 import Web3 from 'web3';
+import json from '../eth.json';
 
 class App extends Component {
 
@@ -23,21 +24,37 @@ class App extends Component {
         this.redirect = this.redirect.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleGetAccounts = this.handleGetAccounts.bind(this);
     }
 
     setWeb3() {
         this.web3 = new Web3();
         this.web3.setProvider(new Web3.providers.HttpProvider("http://10.10.104.37:8545"));
 
-        this.coinbase = this.web3.eth.coinbase;
-        this.balance = this.web3.eth.getBalance(this.coinbase);
+        const myContract = this.web3.eth.contract(json);
+        this.contractInstance = myContract.at('0x7f42f88091b56fe13cc7a0c17cab7997208dc859');
 
+    }
+
+    handleGetAccounts(accounts) {
+        debugger;
+        console.log(accounts);
     }
 
     handleSubmit() {
         this.setState({submitting: true});
-        // Send data
-        console.log(this.state.values);
+        const {name, stenobolon, etilestirol, metribolon} = this.state;
+        this.contractInstance.saveResults(
+            parseInt(name),
+            parseInt(stenobolon),
+            parseInt(etilestirol),
+            parseInt(metribolon),
+            {
+                from: '0x71174fe05F7d03E65e2C894Baa36c09f80F3c02C',
+                to: '0x7f42f88091b56fe13cc7a0c17cab7997208dc859'
+            }).then(() => {
+            this.setState({submitting: false});
+        });
         this.setState({values: {}});
     }
 
@@ -46,9 +63,10 @@ class App extends Component {
     }
 
     redirect(path) {
-        console.log('Web3', this.web3);
-        console.log('Coin base', this.coinbase);
-        console.log('Balance', this.balance);
+        console.log(this.contractInstance.passed(3).toString());
+        console.log(this.contractInstance.getResultDetails(3).toString());
+        //this.web3.eth.getAccounts(this.handleGetAccounts);
+
         this.props.history.push(path);
     }
 
@@ -63,11 +81,13 @@ class App extends Component {
                             <Col span={10} offset="7">
                                 <Input placeholder="Имя" onChange={this.handleChange} name="name"/>
                                 <Input placeholder="Cтенболон" onChange={this.handleChange} name="stenobolon"/>
-                                <Input placeholder="Этилэстренол" onChange={this.handleChange} name="Etilestirol"/>
-                                <Input placeholder="Метриболон" onChange={this.handleChange} name="Metribolon"/>
+                                <Input placeholder="Этилэстренол" onChange={this.handleChange} name="etilestirol"/>
+                                <Input placeholder="Метриболон" onChange={this.handleChange} name="metribolon"/>
                             </Col>
                         </Row>
-                        <button className="button buttonSave" size="large" type="danger" onClick={this.handleSubmit}>Save...</button>
+                        <button className="button buttonSave" size="large" type="danger" onClick={this.handleSubmit}>
+                            Save...
+                        </button>
                     </div>
                     <button className="button buttonRedirect"
                             onClick={() => this.redirect(routesConfig.sportsmen.path)}>Go to Results
