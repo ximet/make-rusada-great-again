8c6c2382e9f83a7580f14399fb1774d33d486d2c
ximet <dq.ximet@gmail.com>
2018-03-17 14:29:00 +0300

    add form to main screen

--- resada/src/components/App.js
+++ resada/src/components/App.js
@@ -3,6 +3,22 @@ import './App.css';
 import logo from '../assets/rings.gif';
 import Button from 'antd/lib/button';
 import 'antd/lib/button/style/index.css';
+import Input from 'antd/lib/input';
+import 'antd/lib/input/style/index.css';
+
+import Form from 'antd/lib/form';
+import 'antd/lib/form/style/index.css';
+
+import Grid from 'antd/lib/grid';
+import 'antd/lib/grid/style/index.css';
+
+import Col from 'antd/lib/col';
+
+
+import Row from 'antd/lib/row';
+
+
+
 import routesConfig from '../routesConfig';
 
 class App extends Component {
@@ -23,6 +39,19 @@ class App extends Component {
 
             <div className="content">
                 <Button type="primary" onClick={() => this.redirect(routesConfig.sportsmen.path)}>Go to Results</Button>
+
+                <div className="form">
+                  <Row gutter={16} align="middle" justify="center">
+                  <Col span={10} offset="7">
+                    <Input placeholder="Имя" />
+                    <Input placeholder="Cтенболон" />
+                    <Input placeholder="Этилэстренол" />
+                    <Input placeholder="Метриболон" />
+                  </Col>
+                  </Row>
+                  <Button className="buttonSave" size="large" type="primary">Save...</Button>
+                </div>
+               
             </div>
         </div>
 
